6c2bf75f31455ae2791a115788a6126a0169ca74
Maksat <54549462+Maksat03@users.noreply.github.com>
2023-09-09 13:52:35 +0600

    project apps added

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from project import settings
+
 urlpatterns = [
     path('djadmin/', admin.site.urls),
     path('', include("pages.urls"))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+        # ...
+    ] + urlpatterns
